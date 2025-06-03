8971ff8bf7a9cfde5b870fd26225b59ffac4ae40
Q-Word <f1101011001@gmail.com>
2025-06-03 11:09:33 +0800

    tambah page pertama livewire

---
+++ resources/views/livewire/absensi.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <div class="container mx-auto bg-sky-500">
+        <h1>Tes</h1>
+        <div>
+            <p>cihuy jadi page nya cuy</p>
+        </div>
+    </div>
+    {{-- In work, do what you enjoy. --}}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Absensi;
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('absensi', Absensi::class)->name('absensi');
+
+});
 
 Route::get('/', function () {
     return view('welcome');
